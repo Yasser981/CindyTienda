5a94e29389c04c57a5af541004dd5495e5c44c08
Yaser918 <yasserespinoza22@gmail.com>
2021-12-06 17:36:15 -0600

    actualizacin

--- app/Services/PagoService.php
+++ app/Services/PagoService.php
@@ -111,7 +111,7 @@ class PagoService
             $impresora = new Printer($connector);
             $impresora->setJustification(Printer::JUSTIFY_CENTER);
             $impresora->setTextSize(1, 1);
-            $img = EscposImage::load('images/logo/VariedadesCindyLogo-cutout.png', false);
+            $img = EscposImage::load('images/logo/VariedadesCindyLogo-cutout.png');
             $impresora->graphics($img);
             $impresora->text("Variedades Cindy\n");
             $impresora->text("RUC 3211105930004N \n");
