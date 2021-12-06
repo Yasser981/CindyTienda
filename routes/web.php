969c8f669fbf2338e019a04815885212d53439e2
Yaser918 <yasserespinoza22@gmail.com>
2021-12-06 16:11:08 -0600

    adminLTE

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,6 @@ Route::middleware('auth')->group(function () {
     
     Route::post('impresion',ImprecionPagoController::class)->name('imprecion.pagos');
 });
+///Auth::routes();
+
+///Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
