b5b6132fcaa09dacd0538c83c309759d2c8155af
Yaser918 <yasserespinoza22@gmail.com>
2021-12-06 17:03:52 -0600

    arreglado el error del logotipo

--- app/Services/PagoService.php
+++ app/Services/PagoService.php
@@ -111,6 +111,8 @@ class PagoService
             $impresora = new Printer($connector);
             $impresora->setJustification(Printer::JUSTIFY_CENTER);
             $impresora->setTextSize(1, 1);
+            $img = EscposImage::load('images/logo/VariedadesCindyLogo-cutout.png', false);
+            $impresora->graphics($img);
             $impresora->text("Variedades Cindy\n");
             $impresora->text("RUC 3211105930004N \n");
             $impresora->text("Teléfono 27222871\n");
